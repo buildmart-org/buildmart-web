3e7296813f6885ecc0d3cba7e0e09487153554f1
Ihar <151664439+RqtHqck@users.noreply.github.com>
2026-03-30 13:12:32 +0300

    feat(categories): add query to find flat categories without joins and files

--- src/entities/category/index.ts
+++ src/entities/category/index.ts
@@ -1,7 +1,15 @@
 import type { Category } from '@/entities/category/model/types/types.ts';
-import { useGetCategoriesQuery } from '@/entities/category/api/categoryApi.ts';
+import {
+  useGetCategoriesQuery,
+  useGetFlatCategoriesQuery,
+} from '@/entities/category/api/categoryApi.ts';
 import { CategoriesList } from '@/entities/category/ui/CategoriesList/CategoriesList.tsx';
 import { mapCategoryToOption } from '@/entities/category/lib/mapCategoryToOption.ts';
 
-export { useGetCategoriesQuery, CategoriesList, mapCategoryToOption };
+export {
+  useGetCategoriesQuery,
+  useGetFlatCategoriesQuery,
+  CategoriesList,
+  mapCategoryToOption,
+};
 export type { Category };

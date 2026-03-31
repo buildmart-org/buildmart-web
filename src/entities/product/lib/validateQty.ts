0119744900b2cbddca8ae6da0667114db89cace8
Ihar <151664439+RqtHqck@users.noreply.github.com>
2026-03-31 13:08:59 +0300

    feat(products): create product detailed page

---
+++ src/entities/product/lib/validateQty.ts
@@ -0,0 +1,17 @@
+import { MAX_PRODUCT_QTY } from '@/entities/product/model/consts/consts.ts';
+
+export const validateQty = (
+  value: string | number | undefined,
+): number | undefined => {
+  if (value === '' || value === undefined || value === null) {
+    return undefined;
+  }
+
+  const num = Number(value);
+
+  if (!isNaN(num) && num > 0 && num <= MAX_PRODUCT_QTY) {
+    return num;
+  }
+
+  return undefined;
+};

f89a5e3d2c9d34b1dc0884554b4a6df01e05a126
Ihar <151664439+RqtHqck@users.noreply.github.com>
2026-04-04 14:50:39 +0300

    fix(store): load fn types update

--- src/entities/cart/model/services/loadCart.ts
+++ src/entities/cart/model/services/loadCart.ts
@@ -2,16 +2,18 @@ import type { CartSchema } from '@/entities/cart/model/types/CartSchema.ts';
 import { LOCAL_STORAGE_CART_KEY } from '@/shared/config';
 import { logger } from '@/shared/lib';
 
+const initialState = { items: [], promocode: null };
+
 export const loadCart = (): CartSchema => {
   try {
     const data = localStorage.getItem(LOCAL_STORAGE_CART_KEY);
-    return data ? JSON.parse(data) : { items: [] };
+    return data ? JSON.parse(data) : initialState;
   } catch (error) {
     logger.error({
       scope: 'cart',
       message: 'Could not parse cart data from localstorage',
       error: error,
     });
-    return { items: [] };
+    return initialState;
   }
 };
